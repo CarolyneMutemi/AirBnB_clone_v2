ea7ded571481c0989cfbb3522ad8145c0a576611
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2023-12-12 14:00:53 +0300

    2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,33 +1,30 @@
 #!/usr/bin/python3
 """
-Has the do_deploy function.
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
 """
 
-import os
-from fabric.api import put, env, run
-
-env.hosts = ['34.229.69.44', '100.26.9.101']
+from fabric.api import put, run, env
+from os.path import exists
+env.hosts = ['142.44.167.228', '144.217.246.195']
 
 
 def do_deploy(archive_path):
-    """
-    Distributes an archive to the web servers.
-    """
-    if not os.path.exists(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
-
     try:
-        put(archive_path, "/tmp/")
-        new_dir = archive_path.strip('.tgzversions/')
-        new = f'/data/web_static/releases/{new_dir}'
-        run(f"mkdir -p {new}")
-        run(f"tar -xzf /tmp/{new_dir}.tgz -C {new}")
-        run(f"mv {new}/web_static/* {new}/")
-        run(f"rm -rf {new}/web_static")
-        run(f"rm /tmp/{new_dir}.tgz")
-        run("rm -rf /data/web_static/current")
-        run(f"ln -s {new} /data/web_static/current")
-        print('New version deployed!')
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
     except:
         return False
