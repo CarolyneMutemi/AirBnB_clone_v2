189239aeccb6f32f6daa324cc6a93a90caa4777d
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2023-12-31 01:50:04 +0300

    Two

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web application
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """
+    Index page.
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    HBNB page.
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def get_text(text):
+    """
+    Get text from URL.
+    """
+    return f"C {text}".replace('_', " ")
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
