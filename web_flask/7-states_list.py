88b8990a8571c69aa669d2cc444cebf6dcad70ed
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2024-01-02 02:58:37 +0300

    Seven

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web application.
+"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+def func(obj):
+    """
+    To sort.
+    """
+    return obj.name
+
+
+@app.route('/states_list', strict_slashes=False)
+def get_states():
+    """
+    The states route.
+    """
+    states = sorted(storage.all(State).values(), key=func)
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
